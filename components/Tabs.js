5490392ecd4c7633b3c5986cccd5537d5bebb175
unknown <Alikhan7273@gmail.com>
2020-07-19 17:05:12 -0500

    done

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,23 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+const Topics = document.querySelector('.topics')
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then (response =>{
+        console.log('response', response)
+
+        response.data.topics.forEach(TrendingTopic => {
+
+            const NewTab = document.createElement('div')
+            NewTab.classList.add('tab')
+            NewTab.textContent = TrendingTopic
+            
+            Topics.appendChild(NewTab)
+        })
+    })
+    .catch(error =>{
+        console.log('ERORRR!!!!!', error)
+    })
